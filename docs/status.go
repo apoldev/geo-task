12c71580861bf54966c12557326d9587040e6412
apoldev <sozdaibk@gmail.com>
2024-01-27 13:29:12 +0300

    Add docs

--- docs/status.go
+++ docs/status.go
@@ -1,3 +1,16 @@
 package docs
 
+import "github.com/GoGerman/geo-task/module/courierfacade/models"
+
 // добавить документацию для роута /api/status
+
+// swagger:route GET /api/status courier GetStatus
+// Get courier status
+// Responses:
+//   200: GetStatusRes200
+
+// swagger:response GetStatusRes200
+type CourierResponse struct {
+	// in:body
+	Body models.CourierStatus
+}
